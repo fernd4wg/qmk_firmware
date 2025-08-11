27e3dc4d5bdbe90c0537dacbc0d0e421f56b0215
ferny <ferdoohd2@gmail.com>
2025-08-10 17:17:09 -0700

    adding new qmk-firmware

---
+++ keyboards/hyprmacroboard/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// matrix size
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 3
+
+#define DIODE_DIRECTION COL2ROW
+
+// encoder details
+#define ENCODER_PAD_A {F6}
+#define ENCODER_PAD_B {F7}
+#define ENCODER_RESOLUTION 4
+
+// oled display
+#define OLED_DISPLAY_128x32
+#define OLED_SDA_PIN D1
+#define OLED_SCL_PIN D0
+
+#define MATRIX_ROW_PINS {B1, B3, B2, B6, B5}
+#define MATRIX_COL_PINS {F4, F5, B4}
+
